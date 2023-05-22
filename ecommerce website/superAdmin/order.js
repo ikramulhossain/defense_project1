b9bf07b026bb87c3812490471e084e31078d0838
IKRAMUL HOSSAIN <71810942+ikramulhossain@users.noreply.github.com>
2023-05-22 23:31:14 +0600

    Add files via upload

---
+++ ecommerce website/superAdmin/order.js
@@ -0,0 +1,50 @@
+const Orders=[
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'76764',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'76764',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'76764',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'76764',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'40764',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'723464',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'56564',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+{
+    productName: 'Foldable mini drone',
+    ProductNumber:'756u4',
+    paymentStatus:'due',
+    shipping:'Pending'
+},
+]
